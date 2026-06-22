21b954ede70c458b2859464248827b096f2ac30e
Samir <finnexmir@gmail.com>
2026-06-22 16:49:23 +0400

    migration: MehkemeIsi case-level Hakim ve IsNomresi (MehkemeIsleri)

---
+++ DataAccess/Migrations/20260622082600_AddMehkemeIsiHakimIsNo.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinNex.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMehkemeIsiHakimIsNo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Hakim",
+                table: "MehkemeIsleri",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IsNomresi",
+                table: "MehkemeIsleri",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hakim",
+                table: "MehkemeIsleri");
+
+            migrationBuilder.DropColumn(
+                name: "IsNomresi",
+                table: "MehkemeIsleri");
+        }
+    }
+}
