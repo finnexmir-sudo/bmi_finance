274a936eb3b8788e761e4992b2f01dd4d598793c
Samir <finnexmir@gmail.com>
2026-06-22 09:14:31 +0400

    migration: AddMehkemeXercler + AddMehkemeSenedler repo-ya əlavə

---
+++ DataAccess/Migrations/20260619070402_AddMehkemeXercler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinNex.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMehkemeXercler : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MehkemeXercler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MehkemeIsiId = table.Column<int>(type: "int", nullable: false),
+                    Mebleg = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Tarix = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Mehkeme = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    YaradilmaTarixi = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    YaradanIcraciId = table.Column<int>(type: "int", nullable: true),
+                    YenileyenIcraciId = table.Column<int>(type: "int", nullable: true),
+                    SilenIcraciId = table.Column<int>(type: "int", nullable: true),
+                    YenilenmeTarixi = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Silinib = table.Column<bool>(type: "bit", nullable: false),
+                    SilinmeTarixi = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MehkemeXercler", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MehkemeXercler_MehkemeIsleri_MehkemeIsiId",
+                        column: x => x.MehkemeIsiId,
+                        principalTable: "MehkemeIsleri",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MehkemeXercler_MehkemeIsiId",
+                table: "MehkemeXercler",
+                column: "MehkemeIsiId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MehkemeXercler");
+        }
+    }
+}
---
+++ DataAccess/Migrations/20260619083355_AddMehkemeSenedler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinNex.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMehkemeSenedler : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MehkemeSenedler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MehkemeIsiId = table.Column<int>(type: "int", nullable: false),
+                    Ad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Novu = table.Column<int>(type: "int", nullable: false),
+                    FaylYolu = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tarix = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    YaradilmaTarixi = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    YaradanIcraciId = table.Column<int>(type: "int", nullable: true),
+                    YenileyenIcraciId = table.Column<int>(type: "int", nullable: true),
+                    SilenIcraciId = table.Column<int>(type: "int", nullable: true),
+                    YenilenmeTarixi = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Silinib = table.Column<bool>(type: "bit", nullable: false),
+                    SilinmeTarixi = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MehkemeSenedler", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MehkemeSenedler_MehkemeIsleri_MehkemeIsiId",
+                        column: x => x.MehkemeIsiId,
+                        principalTable: "MehkemeIsleri",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MehkemeSenedler_MehkemeIsiId",
+                table: "MehkemeSenedler",
+                column: "MehkemeIsiId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MehkemeSenedler");
+        }
+    }
+}
